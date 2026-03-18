87edfa17e2a02ea4180ef1ba9aae9dc69d774151
katianne23 <katianne40@gmail.com>
2026-03-18 11:21:41 -0300

    feat: created people controller and validator

---
+++ src/controllers/pessoaController.js
@@ -0,0 +1,129 @@
+const Pessoa = require("../models/Pessoa");
+const { validatePessoa } = require("../validators/pessoaValidator");
+
+class PessoaController {
+  async create(req, res) {
+    try {
+      const { error, value } = validatePessoa(req.body);
+      if (error) {
+        return res.status(400).json({
+          errors: error.details.map((err) => err.message),
+        });
+      }
+
+      const { email, telefone } = value;
+
+      const pessoaEmail = await Pessoa.findByEmail(email);
+      if (pessoaEmail) {
+        return res.status(400).json({
+          error: "Email já cadastrado",
+        });
+      }
+
+      const pessoaTelefone = await Pessoa.findByTelefone(telefone);
+      if (pessoaTelefone) {
+        return res.status(400).json({
+          error: "Telefone já cadastrado",
+        });
+      }
+
+      const pessoaData = {
+        nome: value.nome,
+        email: value.email,
+        data_nascimento: value.data_nascimento,
+        telefone: value.telefone,
+        endereco: value.endereco,
+      };
+
+      const id = await Pessoa.create(pessoaData);
+      const novaPessoa = await Pessoa.findById(id);
+      return res.status(201).json(novaPessoa);
+    } catch (error) {
+      console.error("Erro ao criar pessoa:", error);
+
+      return res.status(500).json({
+        error: "Erro interno do servidor",
+      });
+    }
+  }
+
+  async findById(req, res) {
+    try {
+      const { id } = req.params;
+      const pessoa = await Pessoa.findById(id);
+
+      if (!pessoa) {
+        return res.status(404).json({ error: "Pessoa não encontrada" });
+      }
+
+      res.json(pessoa);
+    } catch (error) {
+      console.log("Erro ao buscar pessoa:", error);
+      res.status(500).json({ error: "Erro interno do servidor" });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+
+      const pessoaExistente = await Pessoa.findById(id);
+      if (!pessoaExistente) {
+        return res.status(404).json({ error: "Pessoa não encontrada" });
+      }
+
+      const { error } = validatePessoa(req.body);
+      if (error) {
+        return res.status(400).json({
+          error: error.details[0].message,
+        });
+      }
+
+      const { email } = req.body;
+      if (email !== pessoaExistente.email) {
+        const emailExistente = await Pessoa.findByEmail(email);
+        if (emailExistente) {
+          return res.status(400).json({
+            error: "Email já está em uso",
+          });
+        }
+      }
+
+      await Pessoa.update(id, req.body);
+      const pessoaAtualizada = await Pessoa.findById(id);
+
+      res.json(pessoaAtualizada);
+    } catch (error) {
+      console.error("Erro ao atualizar pessoa:", error);
+      res.status(500).json({ error: "Erro interno do servidor" });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+
+      const pessoaExistente = await Pessoa.findById(id);
+      if (!pessoaExistente) {
+        return res.status(404).json({ error: "Pessoa não encontrada" });
+      }
+
+      await Pessoa.delete(id);
+      res.status(204).send();
+    } catch (error) {
+      console.error("Erro ao deletar pessoa:", error);
+      res.status(500).json({ error: "Erro interno do servidor " });
+    }
+  }
+
+  async findAll(req, res) {
+    try {
+      const pessoas = await Pessoa.findAll();
+    } catch (error) {
+      console.error("Erro ao listar pessoas:", error);
+      res.status(500).json({ error: "Erro interno do servidor" });
+    }
+  }
+}
+
+module.exports = new PessoaController();
---
+++ src/validators/pessoaValidator.js
@@ -0,0 +1,39 @@
+const Joi = require("joi");
+
+const pessoaSchema = Joi.object({
+  nome: Joi.string().min(3).max(100).required().message({
+    "string.min": "Nome deve ter no mínimo 3 caracteres",
+    "string.max": "Nome deve ter no máximo 100 caracteres",
+    "any.required": "Nome é obrigatório",
+  }),
+
+  email: Joi.string().email().required().messages({
+    "string.email": "Email inválido",
+    "any.required": "Email é obrigatório",
+  }),
+
+  data_nascimento: Joi.date().iso().max("now").required().messages({
+    "date.base": "Data de nascimento inválida",
+    "date.max": "Data de nascimento não pode ser no futuro",
+    "any.required": "Data de nascimento é obrigatória",
+  }),
+
+  telefone: Joi.string()
+    .pattern(/^\(?\d{2}\)?\s?\d{4,5}-?\d{4}$/)
+    .required()
+    .messages({
+      "string.pattern.base":
+        "Telefone deve conter apenas números (10 ou 11 dígitos)",
+      "any.required": "Telefone é obrigatório",
+    }),
+  endereco: Joi.string().min(10).max(255).required().messages({
+    "string.min": "Endereço muito curto",
+    "string.max": "Endereço muito longo",
+    "any.required": "Endereço é obrigatório",
+  }),
+});
+function validatePessoa(data) {
+  return pessoaSchema.validate(data);
+}
+
+module.exports = { validatePessoa };
